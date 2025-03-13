29cdda13715f3c60949830f32699f224a3eb0d82
Chanel Mulder <chanelm@velocitycubed.co.za>
2025-03-13 08:10:32 +0200

    made team optional

--- packages/features/bookings/types.ts
+++ packages/features/bookings/types.ts
@@ -57,7 +57,7 @@ export type BookerEvent = Pick<
 > & {
   subsetOfUsers: BookerEventUser[];
   showInstantEventConnectNowModal: boolean;
-} & { profile: BookerEventProfile } & { team: Team };
+} & { profile: BookerEventProfile } & { team?: Team };
 
 export type ValidationErrors<T extends object> = { key: FieldPath<T>; error: ErrorOption }[];
 
